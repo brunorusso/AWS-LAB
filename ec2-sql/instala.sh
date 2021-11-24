a6f6b2fbc9be306ceb130839f4a3a82d4f9d2a22
Bruno Tadeu Russo <brusso@gmail.com>
2021-11-24 16:31:13 -0300

    Update instala.sh

--- ec2-sql/instala.sh
+++ ec2-sql/instala.sh
@@ -6,5 +6,7 @@ echo "CREATE USER 'app'@'%' IDENTIFIED BY 'app';" > /tmp/script.sql
 echo "GRANT ALL PRIVILEGES ON * . * TO 'app'@'%';" >> /tmp/script.sql
 echo "FLUSH PRIVILEGES;" >> /tmp/script.sql
 mysql -u root < /tmp/script.sql
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+service mysql restart
 sleep 1
 rm /tmp/script.sql
